ba9bd43ad30ac947d84dd15fe4c318bbd50ab175
ZandyCortez <107449527+ZandyCortez@users.noreply.github.com>
2022-11-17 03:41:40 -0600

    hola
    nnnn

--- Assets/Jammo-Character/Scripts/Hola.cs
+++ Assets/Jammo-Character/Scripts/Hola.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ejemplo1 : MonoBehaviour
+public class Hola : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
