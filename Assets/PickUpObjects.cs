87a9ea52e2766507906586ec3597299e05ca4e40
ElJonathanM <2002dominguezhernandez@gmail.com>
2022-11-17 18:16:18 -0600

    Cambios
    Agregar objetos

--- Assets/PickUpObjects.cs
+++ Assets/PickUpObjects.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class PickUpObjects : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject ObjectToPickUp;
+    public GameObject PickedObject;
+    public Transform interactionZone;
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (ObjectToPickUp != null && ObjectToPickUp.GetComponent<PickableObject>().isPickable == true && PickedObject == null)
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                PickedObject = ObjectToPickUp;
+                PickedObject.GetComponent<PickableObject>().isPickable = false;
+                PickedObject.transform.SetParent(interactionZone);
+                PickedObject.transform.position = interactionZone.position;
+                PickedObject.GetComponent<Rigidbody>().useGravity = false;
+                PickedObject.GetComponent<Rigidbody>().isKinematic = true;
+            }
+        }
+        else if (PickedObject != null)
+        {
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                PickedObject = ObjectToPickUp;
+                PickedObject.GetComponent<PickableObject>().isPickable = true;
+                PickedObject.transform.SetParent(null);
+                PickedObject.GetComponent<Rigidbody>().useGravity = true;
+                PickedObject.GetComponent<Rigidbody>().isKinematic = false;
+                PickedObject = null;
+            }
+        }
     }
 }
+
--- Assets/PickableObject.cs
+++ Assets/PickableObject.cs
@@ -2,16 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PickableObjetcs : MonoBehaviour
+public class PickableObject : MonoBehaviour
 {
     public bool isPickable = true;
 
-    //Cuando el Rigidbody entre al trigger este hara algo, detectara esa colisión
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "PlayerInteractionZone")
         {
-            other.GetComponentInParent<PickObjects>().ObjectToPickUp = this.gameObject;
+            other.GetComponentInParent<PickUpObjects>().ObjectToPickUp = this.gameObject;
         }
     }
 
@@ -19,8 +18,7 @@ public class PickableObjetcs : MonoBehaviour
     {
         if (other.tag == "PlayerInteractionZone")
         {
-            other.GetComponentInParent<PickObjects>().ObjectToPickUp = null;
+            other.GetComponentInParent<PickUpObjects>().ObjectToPickUp = null;
         }
     }
-
 }
